38122409c4315148481212089e68f48063e806d9
Foldear42 <ale.V42@protonmail.com>
2025-02-17 11:31:56 +0100

    first commit

---
+++ http_server.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 5648
+
+int main() {
+    int sockfd; 
+    struct sockaddr_in my_addr;
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // 0 choose the correct protocol based on the type
+
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = inet_addr("10.12.110.57");
+    my_addr.sin_port = htons(PORT);
+    memset(&(my_addr.sin_zero), '/0', 8);
+
+    // Assign an address to the sockfd
+    // The cast is necessary because bind does not accept the struct sockaddr_in 
+    bind(sockfd, (struct sockaddr *) &my_addr, sizeof(my_addr));
+    return 0;
+}
