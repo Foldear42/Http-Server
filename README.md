80bdfcffd33fe03fe3ab1c97c2e3c0daf04c00a2
Foldear42 <ale.V42@protonmail.com>
2025-11-10 09:36:55 +0100

    [RFCT] add docs and remove http_client

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # HTTP Server
+
+This project is an attempt to make a short HTTP server in plain C.
+
+## Features
+
+- [x] Server Connection Handling
+- [ ] HTTP Parser
+    - [ ] Parse request line (method, URI, version)
+	- [ ] Parse HTTP headers
+	- [ ] Validate request format (struct)
+- [ ] HTTP Responses
+	- [x] **200 OK**
+		- [ ] Send a minimal HTML page with correct response headers
+	- [ ] **400 Bad Request**
+	- [ ] **403 Forbidden**
+	- [ ] **404 Not Found**
+	- [ ] **500 Internal Server Error**
+	- [ ] **501 Not Implemented**
+- [ ] File Handling
+	- [ ] Check if requested file exists
+    - [ ] Serve HTML files
+	- [ ] Serve binary files (images, PDFs, etc.)
+	- [ ] Handle subdirectory requests
+- [ ] Permissions and Security
+	- [ ] Implement file access permissions
+	- [ ] Verify paths to prevent directory traversal attacks (`../`)
+	- [ ] Validate requested paths are within allowed directories
+	- [ ] Sanitize file paths	
+- [ ] Concurrency
+	- [ ] Multi-threading support
+	- [ ] Handle multiple simultaneous connections
+	- [ ] Thread-safe resource access
+
+Resources :
+
+- [https://tools.ietf.org/html/rfc1945](https://tools.ietf.org/html/rfc1945)
+- https://www.cs.columbia.edu/~danr/courses/6761/Fall00/hw/pa1/6761-sockhelp.pdf
--- http_server.c
+++ http_server.c
@@ -1,28 +1,65 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-#define PORT 5648
+#include "http_server.h"
 
 int main() {
-    int sockfd; 
+    int sockfd, new_fd, len_msg, bytes_sent; 
     struct sockaddr_in my_addr;
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); // 0 choose the correct protocol based on the type
+    struct sockaddr_in their_addr;
+    struct sigaction sa;
+    int sin_size;
+    int yes=1;
+    char buf[MAXDATASIZE];
 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // 0 choose the correct protocol based on the type
+    if(sockfd == -1){
+        perror("Socket");
+        return -1;
+    }
+    
+    // Allow the program to reuse the port
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+        perror("setsockopt");
+        exit(1);
+    }  
+    
     my_addr.sin_family = AF_INET;
-    my_addr.sin_addr.s_addr = inet_addr("10.12.110.57");
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     my_addr.sin_port = htons(PORT);
     memset(&(my_addr.sin_zero), '\0', 8);
 
     // Assign an address to the sockfd
     // The cast is necessary because bind does not accept the struct sockaddr_in 
-    bind(sockfd, (struct sockaddr *) &my_addr, sizeof(my_addr));
-    // Listen for incoming connection 
-    listen(sockfd, 5);
+    if(bind(sockfd, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1){
+        perror("bind");
+        exit(1);
+    }
     
+    // Listen for incoming connection 
+    if(listen(sockfd, 5) == -1){
+        perror("listen");
+        exit(1);
+    }
+
+    // Main loop : handling connections
+    while(1){
+        sin_size = sizeof(struct sockaddr_in);
+        if((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1){
+            perror("accept");
+            continue;
+        }else{
+            printf("Server: connection from %s\n", inet_ntoa(their_addr.sin_addr));
+        }
+        if(recv(new_fd, buf, MAXDATASIZE-1, 0) == -1){
+            perror("recv");
+        }
+        printf("Received: %s",buf); 
+        char * strToken = strtok(buf, " ");
+        // good format with crlf 
+        char *rep = "HTTP/1.1 200 OK\r\n\r\n";
+        if(send(new_fd, rep, strlen(rep), 0) == -1){
+            perror("send");
+        }
+        close(new_fd);
+    }
+    close(sockfd);
     return 0;
 }
